d14af59120723c9014dc108b6eb94cae5a42b1da
Ivan Danylenko <i.danylenko@mindmarker.com>
2017-02-04 16:38:43 +0100

    Add sample roads

--- src/api/map.php
+++ src/api/map.php
@@ -39,17 +39,167 @@
 	36 - grass with land on the right and on the bottom
 	37 - grass with land on the bottom and on the left
 	38 - grass with land on the left and on the top
+
+	39 - grass with land in the top and the right corner
+	310 - grass with land in the right and the bottom corner
+	311 - grass with land in the bottom and the left corner
+	312 - grass with land in the left and the top corner
+	****************************************************
+	ROADS
+	1 - horizontal
+	2 - vertical
+	3 - bottom to right
+	4 - left to bottom
+	5 - left to top
+	6 - top to right
+	7 - horizontal with exit to top
+	8 - horizontal with exit to bottom
+	9 - vertical with exit to right
+	10 - vertical with exit to left
+	11 - crossroad
 */
 echo json_encode(array(
-	array(13, 111 , 110 , 16, 3 , 3 , 3 , 3 , 3 , 3 , 3 ),
-	array(3 , 14, 12, 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 ),
-	array(25 , 17, 16, 3 , 28 , 21 , 25 , 3 , 3 , 18 , 11),
-	array(26 , 3 , 3 , 3 , 27 , 23 , 26 , 3 , 3 , 14, 1),
-	array(3 , 3 , 18, 11, 15, 3 , 3 , 3 , 3 , 14, 1),
-	array(3 , 18, 112 , 1 , 19 , 15 , 3 , 3 , 3 , 14, 1),
-	array(3, 17, 111, 1, 1, 12, 3, 3, 18, 112, 1),
-	array(3, 3, 14, 1, 110, 16, 3, 18, 112, 1, 1),
-	array(3, 3, 17, 13, 16, 3, 3, 14, 1, 1, 1),
-	array(3 , 3 , 3 , 3 , 3, 3, 3, 17, 13, 13, 111),
-	array(3 , 3 , 3 , 3, 3, 3, 3, 3, 3, 3, 14),
+	array(
+		array('terrain' => 13 ),
+		array('terrain' => 111 ),
+		array('terrain' => 110 ),
+		array('terrain' => 16 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3)
+	),
+	array(
+		array('terrain' => 3 ),
+		array('terrain' => 14 ),
+		array('terrain' => 12 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3)
+	),
+	array(
+		array('terrain' => 25 ),
+		array('terrain' => 17 ),
+		array('terrain' => 16 ),
+		array('terrain' => 3 ),
+		array('terrain' => 28 ),
+		array('terrain' => 21 ),
+		array('terrain' => 25 ),
+		array('terrain' => 3 ),
+		array('terrain' => 3 ),
+		array('terrain' => 18 ),
+		array('terrain' => 11)
+	),
+	array(
+		array('terrain' => 26),
+		array('terrain' => 3, 'road' => 3),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'city' => 'city'),
+		array('terrain' => 27),
+		array('terrain' => 23),
+		array('terrain' => 26),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 14),
+		array('terrain' => 1)
+	),
+	array(
+		array('terrain' => 3, 'road' => 3),
+		array('terrain' => 3, 'road' => 5),
+		array('terrain' => 18),
+		array('terrain' => 11),
+		array('terrain' => 15),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 14),
+		array('terrain' => 1)
+	),
+	array(
+		array('terrain' => 3, 'road' => 2),
+		array('terrain' => 18),
+		array('terrain' => 112),
+		array('terrain' => 1),
+		array('terrain' => 19),
+		array('terrain' => 15),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 14),
+		array('terrain' => 1)
+	),
+	array(
+		array('terrain' => 3, 'road' => 2),
+		array('terrain' => 17),
+		array('terrain' => 111),
+		array('terrain' => 1),
+		array('terrain' => 1),
+		array('terrain' => 12),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 18),
+		array('terrain' => 112),
+		array('terrain' => 1)
+	),
+	array(
+		array('terrain' => 3, 'road' => 2),
+		array('terrain' => 3),
+		array('terrain' => 14),
+		array('terrain' => 1),
+		array('terrain' => 110),
+		array('terrain' => 16),
+		array('terrain' => 3),
+		array('terrain' => 18),
+		array('terrain' => 112),
+		array('terrain' => 1),
+		array('terrain' => 1)
+	),
+	array(
+		array('terrain' => 3, 'road' => 2),
+		array('terrain' => 3),
+		array('terrain' => 17),
+		array('terrain' => 13),
+		array('terrain' => 16),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 14),
+		array('terrain' => 1),
+		array('terrain' => 1),
+		array('terrain' => 1)
+	),
+	array(
+		array('terrain' => 3, 'road' => 2),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 3),
+		array('terrain' => 17),
+		array('terrain' => 13),
+		array('terrain' => 13),
+		array('terrain' => 111)
+	),
+	array(
+		array('terrain' => 3, 'road' => 6),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'road' => 7),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'road' => 1),
+		array('terrain' => 3, 'city' => 'capital'),
+		array('terrain' => 14)
+	),
 ));
