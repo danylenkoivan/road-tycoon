7da0e40ebde0905df97b7b5119b02b0792f432a0
Ivan Danylenko <i.danylenko@mindmarker.com>
2017-02-08 23:36:43 +0100

    Add basic road placing functionality

--- src/map/map.component.ts
+++ src/map/map.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Map, MapService }  from './map.service';
 import { Subscription }       from 'rxjs/Subscription';
 
+declare var $: any
+
 @Component({
   selector: 'app-root',
   templateUrl: './map.component.html',
@@ -12,10 +14,14 @@ import { Subscription }       from 'rxjs/Subscription';
 export class MapComponent implements OnInit, OnDestroy {
 	private map: Map;
 	private sub: Subscription;
+    private roads: number[];
+    private currentlySelectedTile: {i: 0, j: 0 };
 
-	constructor(
-    private service: MapService
-	) { }
+    constructor(
+        private service: MapService,
+	) {
+        this.roads = [1,2,3,4,5,6,7,8,9,10,11];
+    }
 
 	ngOnInit() {
 		this.service.getMap()
@@ -27,6 +33,25 @@ export class MapComponent implements OnInit, OnDestroy {
 	}
 
 	tileClicked(i, j) {
-		alert(i + ", " + j);
+        var self = this;
+        $('#road-selector-dialog').modal('show');
+
+        this.currentlySelectedTile = {
+            i: i,
+            j: j
+        };
 	}
+
+    applyRoad(road) {
+        if (road > 0) {
+            if (this.map[this.currentlySelectedTile.i][this.currentlySelectedTile.j].terrain.toString()[0] == 3) {
+                this.map[this.currentlySelectedTile.i][this.currentlySelectedTile.j].road = road;
+            } else {
+                alert('Cannot place road there');
+            }
+        } else {
+            delete(this.map[this.currentlySelectedTile.i][this.currentlySelectedTile.j].road);
+        }
+        $('#road-selector-dialog').modal('hide');
+    }
 }
