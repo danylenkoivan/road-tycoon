66ba8ad58eede69f15c195b13af121f3fe9c3c0f
Ivan Danylenko <i.danylenko@mindmarker.com>
2017-02-09 23:44:20 +0100

    Add drag support and update road picking UI

--- src/map/map.component.ts
+++ src/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 
 import { Map, MapService }  from './map.service';
 import { Subscription }       from 'rxjs/Subscription';
@@ -7,20 +7,27 @@ declare var $: any
 
 @Component({
   selector: 'app-root',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css'],
+  templateUrl: './templates/map.component.html',
+  styleUrls: ['./styles/map.component.css'],
   providers: [MapService]
 })
 export class MapComponent implements OnInit, OnDestroy {
 	private map: Map;
 	private sub: Subscription;
     private roads: number[];
-    private currentlySelectedTile: {i: 0, j: 0 };
 
     constructor(
         private service: MapService,
+        private ref: ChangeDetectorRef
 	) {
-        this.roads = [1,2,3,4,5,6,7,8,9,10,11];
+        var self = this;
+        this.roads = [3,4,1,6,5,2,8,9,10,7,11];
+
+        // when document is ready - initialize the draggables
+        $(document).ready(function() {
+            self.setDraggableRoadSelector()
+            self.setDraggableRoads();
+        });
     }
 
 	ngOnInit() {
@@ -32,26 +39,66 @@ export class MapComponent implements OnInit, OnDestroy {
 		this.sub.unsubscribe();
 	}
 
-	tileClicked(i, j) {
+    /**
+     * Make all roads in roads selector draggable and can be placed on the map
+     */
+    setDraggableRoadSelector() {
         var self = this;
-        $('#road-selector-dialog').modal('show');
+        $('.road-selector-draggable').draggable({helper:'clone'});
+        $('.tile').droppable({
+            accept: ".road-selector-draggable",
+            drop: function(event,ui) {
+                var i = $(this).data("i");
+                var j = $(this).data("j");
+                self.applyRoad($(ui.draggable).data("road"), i, j);
+            }
+        });
+    }
 
-        this.currentlySelectedTile = {
-            i: i,
-            j: j
-        };
-	}
+    /**
+     * Make all roads placed on the map are draggable and can be removed from the map
+     */
+    setDraggableRoads() {
+        var self = this;
+        $('.placed-road-draggable').draggable({revert: 'invalid'});
+        $('.road-removal').droppable({
+            accept: ".placed-road-draggable",
+            classes: {
+                "ui-droppable-active": "bg-primary",
+            },
+            drop: function(event,ui) {
+                var i = $(ui.draggable).parent().data("i");
+                var j = $(ui.draggable).parent().data("j");
+                self.applyRoad(0, i, j);
+                // remove the element
+                $(ui.draggable).hide("fast", function() {
+                    $(this).remove();
+                });
+            }
+        })
+    }
 
-    applyRoad(road) {
+    /**
+     * Add or remove road from map
+     * @param number road Road identifier
+     * @param number i    i coordinate
+     * @param number j    j coordinate
+     */
+    applyRoad(road, i, j) {
+        this.ref.detach();
         if (road > 0) {
-            if (this.map[this.currentlySelectedTile.i][this.currentlySelectedTile.j].terrain.toString()[0] == 3) {
-                this.map[this.currentlySelectedTile.i][this.currentlySelectedTile.j].road = road;
+            // if it is regular terrain then allow to place the road on that tile
+            if (this.map[i][j].terrain.toString()[0] == 3) {
+                this.map[i][j].road = road;
             } else {
                 alert('Cannot place road there');
             }
         } else {
-            delete(this.map[this.currentlySelectedTile.i][this.currentlySelectedTile.j].road);
+            delete(this.map[i][j].road);
         }
-        $('#road-selector-dialog').modal('hide');
+        // force detect change as angular cannot see changes caused by jquery.ui events
+        this.ref.detectChanges();
+        // renew the draggable for possible new tiles on the map
+        this.setDraggableRoads();
     }
 }
